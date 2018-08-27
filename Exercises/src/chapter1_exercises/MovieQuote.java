4a5c2a3298a8a6f811cd0ad922918638c22f645f
Dibbern <ID129001@davenportschools.org>
2018-08-27 09:43:02 -0500

    Made program say My Favorite Movie Quote

---
+++ Exercises/src/chapter1_exercises/MovieQuote.java
@@ -0,0 +1,11 @@
+package chapter1_exercises;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("My Favorite Movie Quote");
+	}
+
+}
