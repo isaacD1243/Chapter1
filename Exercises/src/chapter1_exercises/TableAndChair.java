fb1f4aacbaef71a9f8c0e245d7b92ddc4457c474
Dibbern <ID129001@davenportschools.org>
2018-08-27 09:32:26 -0500

    Made Table and Chairs and Taco Truck

--- Exercises/src/chapter1_exercises/TableAndChair.java
+++ Exercises/src/chapter1_exercises/TableAndChair.java
@@ -2,4 +2,35 @@ package chapter1_exercises;
 
 public class TableAndChair {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("x                   x");
+		System.out.println("x                   x");
+		System.out.println("x                   x");
+		System.out.println("x                   x");
+		System.out.println("x      xxxxxxxx     x");
+		System.out.println("x      x       x    x");
+		System.out.println("xxxxx  x       x xxxx");
+		System.out.println("x   x           x   x");
+	
+		
+		displayTruck();
+	
+	}
+	
+	
+	public static void displayTruck() {
+	
+		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		System.out.println("xxx            x           x");
+		System.out.println("xxx            x           x");
+		System.out.println("xxx            x           x");
+		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		System.out.println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+		System.out.println("xoooooxxxxxxxxxxxxxxxxoooooxxxxxxxxxx");
+		System.out.println("xoooooxxxxxxxxxxxxxxxxoooooxxxxxxxxxx");
+		System.out.println("xooooox              xooooox");
+
+	}
 }
