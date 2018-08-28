074b57970ed74ebd487876c5924e1d602fa0e8f5
Dibbern <ID129001@davenportschools.org>
2018-08-28 09:26:13 -0500

    Completed Case Problems

---
+++ Exercises/src/CaseProblems/CarlysMotto2.java
@@ -0,0 +1,14 @@
+package CaseProblems;
+
+public class CarlysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("***********************************************");
+		System.out.println("*Carly's makes the food that makes it a party.*");
+		System.out.println("***********************************************");
+		
+	}
+
+}
---
+++ Exercises/src/CaseProblems/SammysMotto2.java
@@ -0,0 +1,13 @@
+package CaseProblems;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+		System.out.println("Sammy's makes it fun in the sun.");
+		System.out.println("SsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSs");
+	}
+
+}
