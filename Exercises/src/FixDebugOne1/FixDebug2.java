3fa90c8910bb6c9ed70e3f8c069ac17cb78cc984
Dibbern <ID129001@davenportschools.org>
2018-08-28 09:08:57 -0500

    Finnished debugging exercises

---
+++ Exercises/src/FixDebugOne1/FixDebug2.java
@@ -0,0 +1,15 @@
+package FixDebugOne1;
+
+public class FixDebug2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Java programming is fun.");
+	      System.out.println("Getting a program to work");
+	      System.out.println("can be a challenge,");
+	      System.out.println("but when everything works correctly,");
+	      System.out.println("it's very satisfying");
+	}
+
+}
---
+++ Exercises/src/FixDebugOne1/FixDebug4.java
@@ -0,0 +1,12 @@
+package FixDebugOne1;
+import javax.swing.JOptionPane;
+public class FixDebug4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	     JOptionPane.showMessageDialog(null, "First GUI program");
+
+	}
+
+}
