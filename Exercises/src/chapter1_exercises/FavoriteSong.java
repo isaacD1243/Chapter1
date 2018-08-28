532392ae55cfaa48a709891766cfbe8599158d8a
Dibbern <ID129001@davenportschools.org>
2018-08-28 08:45:59 -0500

    Made program say 4 lines of my favorite song

--- Exercises/src/chapter1_exercises/FavoriteSong.java
+++ Exercises/src/chapter1_exercises/FavoriteSong.java
@@ -5,6 +5,10 @@ public class FavoriteSong {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.print("Who you think you ahead of Not us\r\n" + 
+				"In the S or the Porsche Carrera or some\r\n" + 
+				"F*** twins like Tia and Tamera somethin'\r\n" + 
+				"Yeah, that boy gettin' bread, Panera, somethin'");
 	}
 
 }
