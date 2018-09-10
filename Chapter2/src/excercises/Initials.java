db9aa6785bd378bcf9e4d3914b3796d330430cfe
Dibbern <ID129001@davenportschools.org>
2018-09-10 09:14:23 -0500

    Worked on assignments

---
+++ Chapter2/src/excercises/Initials.java
@@ -0,0 +1,26 @@
+package excercises;
+
+import java.util.Scanner;
+
+public class Initials {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String initial1;
+		String initial2;
+		
+		 Scanner input = new Scanner(System.in);
+		 
+		System.out.println("What is your first initial? ");
+		initial1  = input.next();
+		
+		System.out.println("What is your second initial? ");
+		initial2 = input.next();
+		
+		
+		
+		
+	}
+
+}
